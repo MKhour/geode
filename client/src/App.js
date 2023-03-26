13c8bdf954eeec3f9ac2e9086299683a45239d7d
Megan Kuo <mlk6une@virginia.edu>
2023-03-25 23:54:46 -0400

    text-entry with submission button

--- client/src/App.js
+++ client/src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {passwords: ["hello"], entry: '', value: ""};
+    this.state = {entry: '', value: "", finished: false};
     // value has to stay, it clears the text area so that a character is only added once
 
     this.handleChange = this.handleChange.bind(this);
@@ -118,7 +118,8 @@ class App extends Component {
     console.log(this.state.entry);
     // entry => this.setState({this.state.entry});
     // alert(`The name you entered was: ${this.state.entry}`)
-    this.setState({entry: this.state.entry});
+    // this.setState({entry: this.state.entry});
+    this.setState({finished: true});
     event.preventDefault();
   }
 
@@ -131,13 +132,14 @@ class App extends Component {
 
   render() {
     const { passwords } = this.state;
+    const { finished } = this.state;
     // const { entry } = this.state;
     // console.log("hello" + this.state.entry);
 
     return (
       <div className="App">
         {/* Render the passwords if we have them */}
-        { passwords.length ? (
+        { !finished ? (
           <div>
             <h1>Geode Journaling</h1>
             <ul>
@@ -154,11 +156,13 @@ class App extends Component {
                 change positions in the array.
               */}
               <form onSubmit={ this.handleSubmit }>
-              <label for="entry">Entry:</label><br></br>
+              {/* <label for="entry">Entry:</label><br></br> */}
               {/* <input type="text" value={this.state.entry} onChange= {this.handleChange} >
                 </input><br></br> */}
-                <p>your entry: {this.state.entry}</p><textarea value={this.state.value} onChange={this.handleChange} />
-              <input type="submit" value="Submit"></input>
+                <p>your entry: <br></br><br></br>
+                {this.state.entry}</p><textarea value={this.state.value} onChange={this.handleChange} />
+                <br></br> 
+                <input type="submit" value="Done editing"></input>
               </form>
             </ul>
             
@@ -167,16 +171,49 @@ class App extends Component {
               onClick={this.getPasswords}>
               Get More
             </button> */}
+
+
           </div>
         ) : (
           // Render a helpful message otherwise
+          // <div>
+          //   <h1>No passwords :(</h1>
+          //   <button
+          //     className="more"
+          //     onClick={this.getPasswords}>
+          //     Try Again?
+          //   </button>
+          // </div>
           <div>
-            <h1>No passwords :(</h1>
-            <button
+            <h1>Geode Journaling</h1>
+            <ul>
+              <li>Made for HooHacks 2023</li>
+              <li>Madelyn K., Catherine X., Megan K.</li>
+            </ul>
+            <h2>Write a journal entry below:</h2><br></br>
+
+            <ul className="passwords">
+              {/*
+                Generally it's bad to use "index" as a key.
+                It's ok for this example because there will always
+                be the same number of passwords, and they never
+                change positions in the array.
+              */}
+                <p>your entry: <br></br><br></br>
+                {this.state.entry}</p>
+            </ul>
+            
+            {/* <button
               className="more"
               onClick={this.getPasswords}>
-              Try Again?
-            </button>
+              Get More
+            </button> */}
+            <ul className="passwords">
+              <p>your results: </p>
+            </ul>
+
+            
+
           </div>
         )}
       </div>
