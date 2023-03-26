ac375f9386344091b882ae4568e64a5f44264407
MKhour <mgkhoury2001@gmail.com>
2023-03-26 08:26:16 -0400

    fixed polyfill issues.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const cohere = require("cohere-ai");
+cohere.init("9d5L8VPsV4SepwOYwBCLWzAMawvGos0LxmsCI4QM");
+const examples = [
+  ({text: "If I don't get an internship, I'll never get a job", label: "Catastrophizing"}),
+  ({text: "I'll never be able to find love", label: "Catastrophizing"}),
+  ({text: "What if I fail my class?", label: "Anxiety"}),
+  ({text: "I can't stop thinking that I might have cancer or another serious disease", label: "Anxiety"}),
+  ({text: "I'm inherently unlikable", label: "Low self esteem"}),
+  ({text: "I'm bad at what I do", label: "Low self esteem"}),
+]
+
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {entry: '', value: "", finished: false,
-      inputs: "No one likes me", classifications: []
-    };
+    this.state = {entry: '', value: "", finished: false, inputs: [], classifications: []};
     // value has to stay, it clears the text area so that a character is only added once
 
     this.handleChange = this.handleChange.bind(this);
@@ -17,12 +26,14 @@ class App extends Component {
 
   // Fetch passwords after first mount
   componentDidMount() {
-    this.classifyText(this.state.inputs);
+    // this.classifyText(this.state.inputs);
   }
 
   handleSubmit(event) {
-    console.log(this.state.entry); // doesn't work but I'll leave it
-    this.setState({finished: true}); 
+    console.log(this.state.entry);
+    this.setState({finished: true, inputs: this.state.entry.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g)}); 
+    // console.log(this.state.entry.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g));
+    this.classifyTextAPI(this.state.entry.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g));
     event.preventDefault();
   }
 
@@ -32,16 +43,35 @@ class App extends Component {
     
   }
 
-  classifyText = (input) => {
+  classifyTextAPI = (input) => {
     console.log("INPUT ARG IN FRONTENT IS:" + input);
     // Get the passwords and store them in state
-    fetch(`/api/classify-text?input=${input}`)
+    fetch('/api/classify-text?' + new URLSearchParams({
+      input: input,
+    }))
       .then(res => res.json())
       .then(classifications => this.setState({ classifications }));
   }
 
+  /*
+  async classifyExamples(input) {
+
+    const response = await cohere.classify({
+      inputs: input,
+      examples: examples,
+    })
+  
+    const classifications = response.body.classifications
+    const predictions = classifications.map(classification => classification.prediction)
+    console.log("reponse from classify examples is: " + predictions)
+    console.log("type in cohere api call is: " + typeof(predictions))
+  
+    return predictions
+  }
+*/
   render() {
     const { finished } = this.state;
+    const {classifications} = this.state;
 
     return (
       <div className="App">
